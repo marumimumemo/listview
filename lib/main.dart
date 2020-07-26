4c14147e04a40e3bf6f4311240a4e733ac582403
marumimumemo <sagaminumber3@yahoo.co.jp>
2020-07-26 19:26:16 +0900

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,51 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('ListView'),
+        ),
+        body: ListView(
+            children: [
+              _menuItem("メニュー1", Icon(Icons.settings)),
+              _menuItem("メニュー2", Icon(Icons.map)),
+              _menuItem("メニュー3", Icon(Icons.room)),
+              _menuItem("メニュー4", Icon(Icons.local_shipping)),
+              _menuItem("メニュー5", Icon(Icons.airplanemode_active)),
+            ]
+        ),
+      ),
+    );
+  }
+
+  Widget _menuItem(String title, Icon icon) {
+    return Container(
+      decoration: new BoxDecoration(
+          border: new Border(bottom: BorderSide(width: 1.0, color: Colors.grey))
+      ),
+      child:ListTile(
+        leading: icon,
+        title: Text(
+          title,
+          style: TextStyle(
+              color:Colors.black,
+              fontSize: 18.0
+          ),
+        ),
+        onTap: () {
+          print("onTap called.");
+        }, // タップ
+        onLongPress: () {
+          print("onLongPress called.");
+        }, // 長押し
+      ),
+    );
+  }
+}
